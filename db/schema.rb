c5bed4afaf149c54e732016de27f859f5cb52148
Jon Atlas <jjjatlas@hotmail.com>
2016-01-07 19:27:11 -0500

    Add Ingredient model functionality

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160107232349) do
+ActiveRecord::Schema.define(version: 20160107235912) do
+
+  create_table "directions", force: :cascade do |t|
+    t.text     "step"
+    t.integer  "recipe_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "directions", ["recipe_id"], name: "index_directions_on_recipe_id"
 
   create_table "ingredients", force: :cascade do |t|
     t.string   "name"
@@ -21,6 +30,15 @@ ActiveRecord::Schema.define(version: 20160107232349) do
   end
 
   add_index "ingredients", ["recipe_id"], name: "index_ingredients_on_recipe_id"
+
+  create_table "items", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "recipe_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "items", ["recipe_id"], name: "index_items_on_recipe_id"
 
   create_table "recipes", force: :cascade do |t|
     t.string   "title"
